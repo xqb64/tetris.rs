95848406d0ed00f09f91f743ced7f13edecf1400
xqb64 <x@xqb.ch>
2021-03-11 11:28:11 +0100

    Improve control flow

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,48 @@ use ncurses as nc;
 mod core;
 mod ui;
 
+const KEY_A: i32 = b'a' as i32;
+const KEY_D: i32 = b'd' as i32;
+const ESC: i32 = 27;
+
 fn main() {
     ui::curses_init();
-    let mut tetromino = core::Tetromino::new();
-    tetromino.pick_random_rotation();
+
+    let (border_screen, inner_screen) = ui::create_screens();
+
+    nc::wtimeout(inner_screen, 100);
+    nc::keypad(inner_screen, true);
+
+    let mut game = core::Game::new();
+
     loop {
-        nc::clear();
-        ui::draw_tetromino(&tetromino);
-        nc::refresh();
-        tetromino.move_down();
+        ui::erase_screens(inner_screen, border_screen);
+        nc::box_(border_screen, 0, 0);
+        ui::draw_tetromino(inner_screen, &game.tetromino);
+        ui::refresh_screens(inner_screen);
+
+        game.handle_falling();
+
+        let user_input = nc::wgetch(inner_screen);
+        match user_input {
+            nc::KEY_LEFT => {
+                game.tetromino.move_sideways(core::Direction::Left);
+            }
+            nc::KEY_RIGHT => {
+                game.tetromino.move_sideways(core::Direction::Right);
+            }
+            nc::KEY_DOWN => {
+                game.tetromino.move_down();
+            }
+            KEY_A => {
+                game.tetromino.rotate(core::Direction::Left);
+            }
+            KEY_D => {
+                game.tetromino.rotate(core::Direction::Right);
+            }
+            ESC => break,
+            _ => {}
+        }
     }
     ui::curses_teardown();
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,9 +1,18 @@
-use crate::core::{Coord, Tetromino};
+use crate::core::{
+    Coord,
+    Tetromino,
+    PLAYGROUND_WIDTH,
+    PLAYGROUND_HEIGHT,
+};
 use ncurses as nc;
+
+const SCREEN_WIDTH: i32 = PLAYGROUND_WIDTH * 2;
+const SCREEN_HEIGHT: i32 = PLAYGROUND_HEIGHT;
 
 pub fn curses_init() {
     nc::setlocale(nc::LcCategory::all, "");
     nc::initscr();
+    nc::nodelay(nc::stdscr(), true);
     nc::curs_set(nc::CURSOR_VISIBILITY::CURSOR_INVISIBLE);
     nc::noecho();
     nc::keypad(nc::stdscr(), true);
@@ -16,7 +25,35 @@ pub fn curses_teardown() {
     nc::endwin();
 }
 
-pub fn draw_tetromino(tetromino: &Tetromino) {
+pub fn create_screens() -> (nc::WINDOW, nc::WINDOW) {
+    let border_screen = nc::subwin(
+        nc::stdscr(),
+        1 + SCREEN_HEIGHT + 1,
+        1 + SCREEN_WIDTH + 1,
+        (nc::LINES() - SCREEN_HEIGHT) / 2 - 1,
+        (nc::COLS() - SCREEN_WIDTH) / 2 - 1,
+    );
+    let inner_screen = nc::subwin(
+        nc::stdscr(),
+        SCREEN_HEIGHT,
+        SCREEN_WIDTH,
+        (nc::LINES() - SCREEN_HEIGHT) / 2,
+        (nc::COLS() - SCREEN_WIDTH) / 2,
+    );
+    (border_screen, inner_screen)
+}
+
+pub fn erase_screens(border_screen: nc::WINDOW, inner_screen: nc::WINDOW) {
+    nc::werase(inner_screen);
+    nc::werase(border_screen);
+    nc::werase(nc::stdscr());
+}
+
+pub fn refresh_screens(inner_screen: nc::WINDOW) {
+    nc::refresh();
+    nc::wrefresh(inner_screen);
+}
+
     let tetrovec = tetromino.shape.to_vec(tetromino.current_rotation.unwrap());
     for (rowidx, row) in tetrovec.iter().enumerate() {
         for (colidx, col) in row.iter().enumerate() {
